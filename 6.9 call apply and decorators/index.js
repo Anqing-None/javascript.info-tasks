c8f6a8a348d71ee3b677d863d5aff1a715f621c4
anqing <xieanqing6@foxmail.com>
2023-07-07 12:00:09 +0800

    finish 1.间谍装饰器.js

--- 6.9 call apply and decorators/index.js
+++ 6.9 call apply and decorators/index.js
@@ -6,17 +6,17 @@ function slowCalc(x) {
 function sleep(second) {
   let now = new Date().getTime();
   let dest = now + second * 1000;
-  while (new Date().getTime() < dest) {}
+  while (new Date().getTime() < dest) { }
 }
 
-function cacheDecorator(fun) {
+function cacheDecorator(fun, ctx = {}) {
   const cache = new Map();
   function wrapedFun(x) {
     console.log(cache);
     if (cache.has(x)) {
       return cache.get(x);
     }
-    const ret = fun(x);
+    const ret = fun.call(ctx, x);
     cache.set(x, ret);
     return ret;
   }
@@ -27,9 +27,16 @@ const worker = {
   a: 1,
   slow(x) {
     sleep(1);
+    console.log('this.a', this.a)
     return x * this.a;
   },
 };
 
-const b = worker.slow(2);
-console.log(b);
+// const b = worker.slow(2);
+// console.log(b);
+
+const workerSlow = cacheDecorator(worker.slow, worker);
+
+workerSlow(2)
+
+debugger;
